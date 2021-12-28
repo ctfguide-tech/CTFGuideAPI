b3cb66e595da862ad99dce7ac68247d7abb5f81d
Laphatize <laphatize@protonmail.com>
2021-12-28 11:29:18 -0500

    Added /challenges/all

--- src/challenge.js
+++ src/challenge.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+let challengeModel = require("../models/challenge.js");
 
+// Fetch Challenges
+router.get("/all", (request, response) => {
+    challengeModel.find({}, (err, challengeData) => {
+        if (!challengeData) return response.status(400).json({
+            "message" : "Hmm, it seems like there are no challenges stored in our database."
+        })
+        return response.status(200).json(challengeData);
+    })
+});
 
 module.exports = router;
